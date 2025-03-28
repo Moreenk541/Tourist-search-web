3e60df87af395b42ffda39fd513f0478dc38a6e0
moreen karimi <moreenk541@gmail.com>
2025-03-28 13:48:31 +0300

    added the todolist and view button

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const resultsContainer = document.getElementById('places');
     const title = document.getElementById('title');
     const searchContainer = document.getElementById('search-container');
-
+    let destinations = [];
     // Fetch data from db.json using json-server
     async function fetchDestinations() {
         try {
             const response = await fetch('http://localhost:3000/destination');
             const data = await response.json();
-            return data;
+            return data; 
+           
         } catch (error) {
             console.error('Error fetching destinations:', error);
         }
+       
     }
 
     // Filter and display destinations
@@ -33,6 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
         displayResults(filteredDestinations)
        
     }
+    
     //display results
     function displayResults(destinations) {
         resultsContainer.innerHTML = '';
@@ -44,20 +47,38 @@ document.addEventListener('DOMContentLoaded', () => {
             const destinationElement = document.createElement('div');
             destinationElement.classList.add('destination')
             destinationElement.innerHTML =`
-                <h2>Name:${dest.name}</h2>
+                <h2>Name:${dest.name}</h2>               
                 <h3>Country:${dest.country}</h3>
                 <p style="font-weight: bold;">Season: ${dest.season}</p>
                 <p style="font-weight: bold;">Interest: ${dest.interest}</p>
+                <img src="${dest.image}" id="imageview" alt="${dest.name} image" style="width: 200px; height: auto; border-radius: 10px;">
+                <button type="submit" class="view-btn">View</button>
+                <button type="submit">To do List</button>
 
 
             `;
             resultsContainer.appendChild(destinationElement)
-        })
+        });
+         
+        resultsContainer.addEventListener("click", function (event) {
+            if (event.target.classList.contains("view-btn")) {
+               const img =document.getElementById('imageview')
+                img.style.display = img.style.display === "none" ? "block" : "none";
+            
+            }
+        });
+
 
     }
+  
     searchForm.addEventListener('submit', search);
 
+    async function init() {
+        destinations = await fetchDestinations();
+        displayResults(destinations); // Show all destinations initially
+    }
 
+    init();
     
 
 
